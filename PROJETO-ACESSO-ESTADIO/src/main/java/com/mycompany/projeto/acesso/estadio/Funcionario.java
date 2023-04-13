4c0985103f87deab50eb5e00b971cf158adb9a03
LucasLeopolski <lleopolski@gmail.com>
2023-04-12 21:23:32 -0300

    TODAS CLASSES

---
+++ PROJETO-ACESSO-ESTADIO/src/main/java/com/mycompany/projeto/acesso/estadio/Funcionario.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto.acesso.estadio;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lucas
+ */
+public class Funcionario {
+    
+public int id;
+    public String nome;
+    public int tempodeTrabalho;
+    public String funcao;
+    public String cpf;
+    public String endereco;
+    public String sexo;
+    public int idade;
+    public String setor;
+     List<Funcionario> lista = new ArrayList<>();
+    
+    
+    Funcionario[] funcionarios = new Funcionario[3];
+    
+    public Funcionario() {
+        this.id = 0;
+        this.nome = "";
+        this.tempodeTrabalho = 0;
+        this.funcao = "";
+        this.cpf = "";
+        this.endereco = "";
+        this.sexo = "";
+        this.idade = 0;
+        this.setor = "";
+    
+    }
+    public Funcionario(int id, String nome, int tempodeTrabalho, String funcao, String cpf, String endereco, String sexo, int idade, String setor) {
+        this.id = id;
+        this.nome = nome;
+        this.tempodeTrabalho = tempodeTrabalho;
+        this.funcao = funcao;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.setor = setor;
+        
+    }
+
+     public void removerFuncionario() {
+        System.out.println("Funcionário removido com sucesso!");
+        
+    }
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTempodeTrabalho() {
+        return tempodeTrabalho;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+    
+    public static Funcionario[] removerFuncionario(Funcionario[] funcionarios, int id) {
+    List<Funcionario> listaAtualizada = new ArrayList<>();
+    for (Funcionario funcionario : funcionarios) {
+        if (funcionario.getId() != id) {
+            listaAtualizada.add(funcionario);
+        }
+    }
+    return listaAtualizada.toArray(new Funcionario[listaAtualizada.size()]);
+
+}
+    
+    public static Funcionario[] atualizarFuncionario(Funcionario[] funcionarios, Funcionario funcionarioAtualizar, int novoId, String novoNome, int novoTempodeTrabalho, String novoFuncao, String novoCpf, String novoEndereco, String novoSexo, int novoIdade, String novoSetor) {
+        int index = -1;
+        for (int i = 0; i < funcionarios.length; i++) {
+            if (funcionarios[i] == funcionarioAtualizar) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            Funcionario funcionarioAtualizado = new Funcionario(novoId, novoNome, novoTempodeTrabalho, novoFuncao, novoCpf, novoEndereco, novoSexo, novoIdade, novoSetor);
+            funcionarios[index] = funcionarioAtualizado;
+        }
+        return funcionarios;
+    }
+
+    
+}
---
+++ PROJETO-ACESSO-ESTADIO/src/main/java/com/mycompany/projeto/acesso/estadio/Menu.java
@@ -0,0 +1,165 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.projeto.acesso.estadio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Menu {
+
+    public static void main(String[] args) {
+        //Bilheteria vender = new Bilheteria();
+        //Funcionario removerFuncionario = new Funcionario(1, "Carla", 7, "Empregada", "556.632.255-32", "Rua Campinas, 88", "F", 26, "Topo do estádio");
+        //Funcionario op = new Funcionario(2, "SÉRGiO", 10, "Empregado", "321.998.124-77", "Rua Godfrey, 63", "M", 31, "Subsolo");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nBem viando ao U.P. STADIUM\n");
+            System.out.println("Por favor digite a opção que você deseja: \n");
+            System.out.println("[1]  BILHETERIA       [2] PESSOA             [3] JOGADOR");
+            System.out.println("[4]  JUIZ             [5] TORCEDOR           [6] CIDADE");
+            System.out.println("[7]  ESTADIO          [8] TIME               [9] JOGO");
+            System.out.println("[10] VENDEDOR         [11] FUNCIONARIO       [12] ACESSO");
+            System.out.println("[13] PLACAR           [14] SAIR");
+            Scanner ler = new Scanner(System.in);
+
+            String escolha = ler.next();
+
+            String Bilheteria = "BILHETERIA ";
+            String Pessoa = "PESSOA";
+            String Jogador = "JOGADOR";
+            String Juiz = "JUIZ";
+            String Torcedor = "TORCEDOR";
+            String Cidade = "CIDADE";
+            String Estadio = "ESTADIO";
+            String Time = "TIME";
+            String Jogo = "JOGO";
+            String Vendedor = "VENDEDOR";
+            String Funcionario = "FUNCIONARIO";
+            String Acesso = "ACESSO";
+            String Placar = "Placar";
+            String Sair = "Sair";
+
+            switch (escolha) {
+                case "1":
+                    System.out.println("SUA ESCOLHA FOI: " + Bilheteria);
+                    
+                    Bilheteria vender = new Bilheteria();
+                    vender.venderIngressos(200.58, 100.29, 850.69);
+                    vender.validarIngressoPorPessoa();
+                    break;
+                case "2":
+                    System.out.println("SUA ESCOLHA FOI: " + Pessoa);
+                    
+
+                    break;
+                case "3":
+                    System.out.println("SUA ESCOLHA FOI: " + Jogador);
+                    
+
+                    Jogador apto = new Jogador(1, 12255, "1.80", 28, "80 Kg", "Atacante", "Santos FC", (float) 158.0);
+                    apto.machucado();
+
+                    break;
+                case "4":
+                    System.out.println("SUA ESCOLHA FOI: " + Juiz);
+                    Juiz cadastrar = new Juiz("AAA", "AAAA", "11122255544", "11114445577", "42999999999", 1434.60);
+                    cadastrar.CadastrarJuiz();
+
+                    
+                    break;
+                case "5":
+                    System.out.println("SUA ESCOLHA FOI: " + Torcedor);
+                    
+                    break;
+                case "6":
+                    System.out.println("SUA ESCOLHA FOI: " + Cidade);
+                    
+                    break;
+                //Cidades mostrar = new Cidades("", "", "", ""); erro desconhecido *lucas*
+
+                case "7":
+                    System.out.println("SUA ESCOLHA FOI: " + Estadio);
+                    
+                    break;
+                case "8":
+                    System.out.println("SUA ESCOLHA FOI: " + Time);
+                    
+                    break;
+
+                case "9":
+                    System.out.println("SUA ESCOLHA FOI: " + Jogo);
+                    
+                    break;
+
+                case "10":
+                    System.out.println("SUA ESCOLHA FOI: " + Vendedor);
+                    
+                    break;
+
+                case "11":
+
+                    System.out.println("SUA ESCOLHA FOI: " + Funcionario);
+                    
+                    Funcionario po = new Funcionario(1, "João", 5, "Lixeiro", "585.336.121-11", "Rua Frork, 14", "M", 55, "Ao redor do estádio");
+                    Funcionario lo = new Funcionario(2, "Carla", 7, "Empregada", "556.632.255-32", "Rua Campinas, 88", "F", 26, "Topo do estádio");
+                    Funcionario op = new Funcionario(3, "Sérgio", 10, "Empregado", "321.998.124-77", "Rua Godfrey, 63", "M", 31, "Subsolo");
+
+                    // System.out.println("[1]Atualizar Funcionario     [2]Remover Funcionário");
+                    //String escolher = ler.next();
+                    
+                    //if(escolher.equals("1")){
+                    //     System.out.println("voce escolheu atualizar funcionario");
+                    //}
+                    
+                    System.out.println("Deseja remover um funcionário? (S/N)");
+                    String opcaoRemover = ler.next();
+
+                    if (opcaoRemover.equals("S")) {
+                        System.out.println("Qual fucionário deseja remover?: [1]João    [2]Carla     [3]Sérgio");
+                        opcaoRemover = ler.next();
+                    }
+                    if (opcaoRemover.equals("1")) {
+                        System.out.println("João foi removido");
+                        po.removerFuncionario();
+                    }
+                    if ((opcaoRemover.equals("2"))) {
+                        System.out.println("Carla foi removida");
+                        lo.removerFuncionario();
+                    }
+                    if ((opcaoRemover.equals("3"))) {
+                        System.out.println("Sérgio foi removido");
+                        op.removerFuncionario();
+                    } else {
+                        System.out.println("Você decidiu não remover um funcionário");
+                    }
+                    
+                    break;
+
+                case "12":
+                    System.out.println("SUA ESCOLHA FOI "+Acesso);
+                    
+                    break; 
+                    
+                case "13":
+                    System.out.println("SUA ESCOLHA FOI: " + Placar);
+                    
+
+                    break;
+                case "14":
+                    System.out.println("Sua escolha foi: " + Sair);
+                    
+                    break;
+
+                default:
+                    System.out.println("\n\nA OPÇÃO DIGITADA É INVALIDA, INFORME UM NUMERO DE 1 Á 12!!\n\n");
+                    i = 0;
+                    break;
+            }
+        }
+
+    }
+}
