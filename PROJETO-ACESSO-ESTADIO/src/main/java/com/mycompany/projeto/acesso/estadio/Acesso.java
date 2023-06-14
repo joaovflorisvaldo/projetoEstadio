8eb6d555537a8d53f535fcd8cf4007f4745a50c0
Maria Eduarda Nava <114370938+meduardanava@users.noreply.github.com>
2023-06-14 02:13:25 -0300

    Add files via upload

--- PROJETO-ACESSO-ESTADIO/src/main/java/com/mycompany/projeto/acesso/estadio/Acesso.java
+++ PROJETO-ACESSO-ESTADIO/src/main/java/com/mycompany/projeto/acesso/estadio/Acesso.java
@@ -1,166 +1,29 @@
 package com.mycompany.projeto.acesso.estadio;
 
-public class Acesso extends Bilheteria{
+import java.util.Scanner;
 
-    private int acessoInteiras;
-    private int acessoMeiaEntrada;
-    private int acessoVip;
+public class Acesso{
+
+    private int entrada;
     
-    public Acesso(int acessoInteiras, int acessoMeiaEntrada, int acessoVip, int totalDisponiveis, int inteirasVendidas, int meiaEntradasVendidas, int vipVendidas, int entradasVendidas, double valorInteira, double valorMeiaEntrada, double valorVip, double valorTotal) {
-        super(totalDisponiveis, inteirasVendidas, meiaEntradasVendidas, vipVendidas, entradasVendidas, valorInteira, valorMeiaEntrada, valorVip, valorTotal);
-        this.acessoInteiras = acessoInteiras;
-        this.acessoMeiaEntrada = acessoMeiaEntrada;
-        this.acessoVip = acessoVip;
-    }
-
-    public int getAcessoInteiras() {
-        return acessoInteiras;
-    }
-
-    public void setAcessoInteiras(int acessoInteiras) {
-        this.acessoInteiras = acessoInteiras;
-    }
-
-    public int getAcessoMeiaEntrada() {
-        return acessoMeiaEntrada;
-    }
-
-    public void setAcessoMeiaEntrada(int acessoMeiaEntrada) {
-        this.acessoMeiaEntrada = acessoMeiaEntrada;
-    }
-
-    public int getAcessoVip() {
-        return acessoVip;
-    }
-
-    public void setAcessoVip(int acessoVip) {
-        this.acessoVip = acessoVip;
+    public void validarAcesso(){
+        
+        Scanner ler = new Scanner(System.in);
+        Bilheteria bilheteria = new Bilheteria();
+        
+        do{
+            System.out.println("\nINFORME O CÓDIGO DA ENTRADA");
+            entrada = ler.nextInt();
+            if(entrada == bilheteria.getCodigoEntrada()){
+                System.out.println("\nENTRADA AUTORIAZADA!");
+            }else{
+                System.out.println("\nENTRADA NEGADA, TENTE NOVAMENTE!");
+            }
+        }while(false);
     }
     
 }
+
     
-    /*public static void main(String[] args) {
-        Scanner n = new Scanner(System.in);
-        Acesso acesso = new Acesso();
-        Calendar horaAtual = Calendar.getInstance();
-        Calendar dataAtual = Calendar.getInstance();
-        SimpleDateFormat formatarData = new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat formatarHora = new SimpleDateFormat("HH:mm:ss");
 
-        System.out.println("Usuário de Acesso: ");
-        acesso.setQuemAcessa(n.nextLine());
-        
-        System.out.println("Dia de Acesso: " + formatarData.format(dataAtual.getTime()));
-        acesso.setDiaAcesso(dataAtual);
-        
-        System.out.println("Horário de Acesso: " + formatarHora.format(horaAtual.getTime()));
-        acesso.setHoraAcesso(horaAtual.get(Calendar.HOUR_OF_DAY));
-        
-        System.out.println("Nome do comprador: ");
-        acesso.setNome(n.nextLine());
-        
-        System.out.println("CPF do comprador: ");
-        acesso.setCpf(n.nextLine());
-        
-        System.out.println("Data de nascimento do comprador (dd/mm/yyyy): ");
-        Calendar nascimento = Calendar.getInstance();
-        nascimento.setTimeInMillis(n.nextLong());
-        acesso.setNascimento(nascimento);
-        
-        System.out.println("Selecione o tipo de ingresso (VIP ou NORMAL):");
-        String tipoIngresso = n.nextLine().toUpperCase();
-        
-        while (!tipoIngresso.equals("VIP") && !tipoIngresso.equals("NORMAL")) {
-            System.out.println("Selecione o tipo de ingresso (VIP ou NORMAL):");
-            tipoIngresso = n.nextLine().toUpperCase();
-        }
-        acesso.validarAcesso();
-        acesso.imprimirDadosAcesso();
-    }
-
-    public void validarAcesso() {
-        if (quemAcessa == null || diaAcesso == null || nome == null || cpf == null || nascimento == null) {
-            System.out.println("Acesso negado. Todos os atributos obrigatórios devem ser informados.");
-            acessoPermitido = false;
-        } else {
-            acessoPermitido = true;
-        }
-    }
-    public void imprimirDadosAcesso() {
-        if (acessoPermitido == true) {
-            System.out.println("Usuário de Acesso: " + quemAcessa);
-            System.out.println("Dia de Acesso: " + diaAcesso.getTime());
-            System.out.println("Horário de Acesso: " + horaAcesso);
-            System.out.println("Nome do comprador: " + nome);
-            System.out.println("CPF do comprador: " + cpf);
-            System.out.println("Data de nascimento do comprador: " + nascimento.getTime());
-            System.out.println("ID do comprador: " + idPessoa);
-            System.out.println("Tipo do Ingresso selecionado: " + tipoIngresso);
-        }
-    }
-    
-    public String getQuemAcessa() {
-        return quemAcessa;
-    }
-
-    public void setQuemAcessa(String quemAcessa) {
-        this.quemAcessa = quemAcessa;
-    }
-
-    public Calendar getDiaAcesso() {
-        return diaAcesso;
-    }
-
-    public void setDiaAcesso(Calendar diaAcesso) {
-        this.diaAcesso = diaAcesso;
-    }
-
-    public int getHoraAcesso() {
-        return horaAcesso;
-    }
-
-    public void setHoraAcesso(int horaAcesso) {
-        this.horaAcesso = horaAcesso;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public Calendar getNascimento() {
-    return nascimento;
-}
-
-public void setNascimento(Calendar nascimento) {
-    this.nascimento = nascimento;
-}
-
-public int getIdPessoa() {
-    return idPessoa;
-}
-
-public void setIdPessoa(int idPessoa) {
-    this.idPessoa = idPessoa;
-}
-
-public enum TipoIngresso {
-    VIP,
-    NORMAL,
-}
-
-public TipoIngresso getTipoIngresso() {
-        return tipoIngresso;
-}
-*/
 
--- PROJETO-ACESSO-ESTADIO/src/main/java/com/mycompany/projeto/acesso/estadio/Bilheteria.java
+++ PROJETO-ACESSO-ESTADIO/src/main/java/com/mycompany/projeto/acesso/estadio/Bilheteria.java
@@ -1,30 +1,22 @@
 package com.mycompany.projeto.acesso.estadio;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bilheteria {
+    
+    Scanner ler = new Scanner(System.in);
     
     private int totalDisponiveis;
     private int inteirasVendidas;
     private int meiaEntradasVendidas;
     private int vipVendidas;
+    private int codigoEntrada;
     public int entradasVendidas;
     public double valorInteira;
     public double valorMeiaEntrada;
     public double valorVip;
     public double valorTotal;
-
-    public Bilheteria(int totalDisponiveis, int inteirasVendidas, int meiaEntradasVendidas, int vipVendidas, int entradasVendidas, double valorInteira, double valorMeiaEntrada, double valorVip, double valorTotal) {
-        this.totalDisponiveis = totalDisponiveis;
-        this.inteirasVendidas = inteirasVendidas;
-        this.meiaEntradasVendidas = meiaEntradasVendidas;
-        this.vipVendidas = vipVendidas;
-        this.entradasVendidas = entradasVendidas;
-        this.valorInteira = valorInteira;
-        this.valorMeiaEntrada = valorMeiaEntrada;
-        this.valorVip = valorVip;
-        this.valorTotal = valorTotal;
-    }
 
     public int getTotalDisponiveis() {
         return totalDisponiveis;
@@ -53,9 +45,17 @@ public class Bilheteria {
     public int getVipVendidas() {
         return vipVendidas;
     }
-
+    
     public void setVipVendidas(int vipVendidas) {
         this.vipVendidas = vipVendidas;
+    }
+    
+    public int getCodigoEntrada() {
+        return codigoEntrada;
+    }
+    
+    public void setCodigoEntrada(int codigoEntrada) {
+        this.codigoEntrada = codigoEntrada;
     }
     
     public void ingressosVendidos(){
@@ -65,148 +65,157 @@ public class Bilheteria {
     public void valorArrecadado(){
         this.valorTotal = (this.inteirasVendidas * this.valorInteira) + (this.meiaEntradasVendidas * this.valorMeiaEntrada) + (this.vipVendidas * this.valorVip);
     }
-    
+
     public void comprarEntrada(){ 
         
         String nome;
         String cpf;
         String setorTorcida = "Casa ou Visitante"; 
         int torcedor;
+        int opcao;
         
-        Scanner ler = new Scanner(System.in);
+        Random codigo = new Random();
+        codigoEntrada = codigo.nextInt(1000);
         
-        System.out.println("CONFORME A TABELA INFORME QUAL ENTRADA DESEJA COMPRAR");
-        System.out.println("[1]INTEIRA: ");
-        System.out.println("[2]MEIA-ENTRADA: ");
-        System.out.println("[3]ENTRADA VIP: ");
+        do{
+            System.out.println("\nCONFORME A TABELA INFORME QUAL ENTRADA DESEJA COMPRAR");
+            System.out.println("[1]INTEIRA: ");
+            System.out.println("[2]MEIA-ENTRADA: ");
+            System.out.println("[3]ENTRADA VIP: ");
+            System.out.println("[0]SAIR");
         
-        int opcao = ler.nextInt();
-        
-        switch (opcao){
-            case 1:
-                System.out.println("VOCÊ SELECIONOU A OPÇÃO INTEIRA, PARA CONTINUAR INFORME SEUS DADOS");
-                System.out.println("NOME: ");
-                nome = ler.next();
-                System.out.println("CPF: ");
-                cpf = ler.next();
-                System.out.println("TORCEDOR DO TIME DA CASA OU VISITANTE?");
-                System.out.println("[4]TIME DA CASA");
-                System.out.println("[5]TIME VISITANTE");
-                torcedor = ler.nextInt();
+            opcao = ler.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("\nVOCÊ SELECIONOU A OPÇÃO INTEIRA, PARA CONTINUAR INFORME SEUS DADOS");
+                    System.out.println("NOME: ");
+                    nome = ler.next();
+                    System.out.println("CPF: ");
+                    cpf = ler.next();
+                    System.out.println("\nTORCEDOR DO TIME DA CASA OU VISITANTE?");
+                    System.out.println("[1]TIME DA CASA");
+                    System.out.println("[2]TIME VISITANTE");
+                    torcedor = ler.nextInt();
                 
-            switch (torcedor) {
-                case 4:
-                    setorTorcida = "Casa";
+                    switch (torcedor) {
+                        case 1:
+                            setorTorcida = "Casa";
+                            break;
+                        case 2:
+                            setorTorcida = "Visitante";
+                            break;
+                        default:
+                            System.out.println("OPÇÃO INVÁLIDA");
+                            break;
+                    }
+                    
+                    System.out.println("\n*ENTRADA COMPRADA COM SUCESSO*");
+                    System.out.println("\nDados:");
+                    System.out.println("Nome\t" + nome);
+                    System.out.println("CPF\t" + cpf);
+                    System.out.println("Setor\t" + setorTorcida);
+                    System.out.println("Código do ingresso\t" + codigoEntrada);
                     break;
                 case 2:
-                    setorTorcida = "Visitante";
-                    break;
-                default:
-                    System.out.println("OPÇÃO INVÁLIDA");
-                    break;
-            }
+                    System.out.println("\nVOCÊ SELECIONOU A OPÇÃO INTEIRA, PARA CONTINUAR INFORME SEUS DADOS");
+                    System.out.println("NOME: ");
+                    nome = ler.next();
+                    System.out.println("CPF: ");
+                    cpf = ler.next();
+                    System.out.println("\nTORCEDOR DO TIME DA CASA OU VISITANTE?");
+                    System.out.println("[1]TIME DA CASA");
+                    System.out.println("[2]TIME VISITANTE");
+                    torcedor = ler.nextInt();
                 
-                System.out.println("*ENTRADA COMPRADA COM SUCESSO*");
-                System.out.println("Dados:");
-                System.out.println("Nome\t" + nome);
-                System.out.println("CPF\t" + cpf);
-                System.out.println("Setor\t" + setorTorcida);
-                break;
-            case 2:
-                System.out.println("VOCÊ SELECIONOU A OPÇÃO INTEIRA, PARA CONTINUAR INFORME SEUS DADOS");
-                System.out.println("NOME: ");
-                nome = ler.next();
-                System.out.println("CPF: ");
-                cpf = ler.next();
-                System.out.println("TORCEDOR DO TIME DA CASA OU VISITANTE?");
-                System.out.println("[4]TIME DA CASA");
-                System.out.println("[5]TIME VISITANTE");
-                torcedor = ler.nextInt();
+                    switch (torcedor) {
+                        case 1:
+                            setorTorcida = "Time da Casa";
+                            break;
+                        case 2:
+                            setorTorcida = "Time Visitante";
+                            break;
+                        default:
+                            System.out.println("OPÇÃO INVÁLIDA");
+                            break;
+                }
+                    
+                    System.out.println("\n*ENTRADA COMPRADA COM SUCESSO*");
+                    System.out.println("\nDados:");
+                    System.out.println("Nome\t" + nome);
+                    System.out.println("CPF\t" + cpf);
+                    System.out.println("Setor\t" + setorTorcida);
+                    System.out.println("Código do ingresso\t" + codigoEntrada);
+                    break;
+                case 3:
+                    System.out.println("\nVOCÊ SELECIONOU A OPÇÃO INTEIRA, PARA CONTINUAR INFORME SEUS DADOS");
+                    System.out.println("NOME: ");
+                    nome = ler.next();
+                    System.out.println("CPF: ");
+                    cpf = ler.next();
+                    System.out.println("\nTORCEDOR DO TIME DA CASA OU VISITANTE?");
+                    System.out.println("[1]TIME DA CASA");
+                    System.out.println("[2]TIME VISITANTE");
+                    torcedor = ler.nextInt();
                 
-            switch (torcedor) {
-                case 4:
-                    setorTorcida = "Time da Casa";
+                    switch (torcedor) {
+                        case 1:
+                            setorTorcida = "Time da Casa";
+                            break;
+                        case 2:
+                            setorTorcida = "Time Visitante";
+                            break;
+                        default:
+                            System.out.println("OPÇÃO INVÁLIDA");
+                            break;
+                        }
+                    
+                    System.out.println("\n*ENTRADA COMPRADA COM SUCESSO*");
+                    System.out.println("\nDados:");
+                    System.out.println("Nome\t" + nome);
+                    System.out.println("CPF\t" + cpf);
+                    System.out.println("Setor\t" + setorTorcida);
+                    System.out.println("Código do ingresso\t" + codigoEntrada);
+                    break;
+                case 0 :
+                    System.out.println("\nVOCÊ ESCOLHEU SAIR DA BILHETERIA.");
+                    break;
+                default :
+                    System.out.println("\nOPÇÃO INVÁLIDA.");
+                    break;
+            }   
+        }while(opcao != 0);   
+    } 
+    
+    public void menuBilheteria(){
+        
+        int opcao;
+        
+        do{
+            System.out.println("\nINFORME O QUE DESEJA FAZER NA BILHETERIA");
+            System.out.println("[1]COMPRAR INGRESSO");
+            System.out.println("[2]INGRESSOS VENDIDOS");
+            System.out.println("[3]VALOR ARRECADADO");
+            System.out.println("[0]SAIR");
+            opcao = ler.nextInt();
+        
+            switch(opcao){
+                case 1:
+                    comprarEntrada();
                     break;
                 case 2:
-                    setorTorcida = "Time Visitante";
+                    ingressosVendidos();
+                    break;
+                case 3:
+                    valorArrecadado();
+                    break;
+                case 0:
+                    System.out.println("\nVOCÊ ESCOLHEU SAIR DA MENU DA BILHETERIA");
                     break;
                 default:
-                    System.out.println("OPÇÃO INVÁLIDA");
-                    break;
+                    System.out.println("\nOPÇÃO INVÁLIDA!");
+                    break; 
             }
-                
-                System.out.println("*ENTRADA COMPRADA COM SUCESSO*");
-                System.out.println("Dados:");
-                System.out.println("Nome\t" + nome);
-                System.out.println("CPF\t" + cpf);
-                System.out.println("Setor\t" + setorTorcida);
-                break;
-            case 3:
-                                System.out.println("VOCÊ SELECIONOU A OPÇÃO INTEIRA, PARA CONTINUAR INFORME SEUS DADOS");
-                System.out.println("NOME: ");
-                nome = ler.next();
-                System.out.println("CPF: ");
-                cpf = ler.next();
-                System.out.println("TORCEDOR DO TIME DA CASA OU VISITANTE?");
-                System.out.println("[4]TIME DA CASA");
-                System.out.println("[5]TIME VISITANTE");
-                torcedor = ler.nextInt();
-                
-            switch (torcedor) {
-                case 4:
-                    setorTorcida = "Time da Casa";
-                    break;
-                case 2:
-                    setorTorcida = "Time Visitante";
-                    break;
-                default:
-                    System.out.println("OPÇÃO INVÁLIDA");
-                    break;
-            }
-                
-                System.out.println("*ENTRADA COMPRADA COM SUCESSO*");
-                System.out.println("Dados:");
-                System.out.println("Nome\t" + nome);
-                System.out.println("CPF\t" + cpf);
-                System.out.println("Setor\t" + setorTorcida);
-                break;
-            default :
-                System.out.println("OPÇÃO INVÁLIDA.");
-                break;
-        }
+        }while(opcao != 0);
     }
 }
-    /*
-        if (opcao.equals("1")) {
-            System.out.println("\nVocê selecionou ingresso normal!");
-
-        } else if (resultado.equals(("2"))) {
-            System.out.println("\nVocê selecionou meia entrada!");
-        } else if (resultado.equals("3")) {
-            System.out.println("\nVocê selecionou ingrsso VIP!");
-        } else {
-            System.out.println("\nSelecione uma opção válida.");
-        }
-   
-
-
-    public void validarIngressoPorPessoa() {
-        for (int i = 2; i > 0; i--) {
-
-            Scanner read = new Scanner(System.in);
-
-            System.out.println("Quantos ingressos você quer comprar? ");
-
-            int quantidade = read.nextInt();
-
-            if (quantidade > 5) {
-                System.out.println("\nVocê excedeu a quantidade de ingressos por pessoa!!(Selecione uma quantidade inferior a 5)\n");
-                i = 2;
-
-            } else {
-                System.out.println("Você selecionou " + quantidade + " Ingresso(s)");
-
-            }
-        }
-    }
-    */
